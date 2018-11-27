609748cb7346a475314f44bf713966603e969258
Noby Fujioka <n.fujioka@abacusmaths.info>
2018-11-27 05:22:32 +0000

    method polymorphism

---
+++ src/OverloadingMain.java
@@ -0,0 +1,28 @@
+// Polymorphism
+
+public class OverloadingMain {
+
+    public void add() {
+        System.out.println("Output: " + 2);
+    }
+
+    public void add(int a) {
+        System.out.println("Output: " + (a+a));
+    }
+
+    public void add(int a, int b) {
+        System.out.println("Output: " + (a+b));
+    }
+
+    public void add(int a, int b, int c) {
+        System.out.println("Output: " + (a+b+c));
+    }
+
+    public static void main(String[] args) {
+        OverloadingMain overloadingMain = new OverloadingMain();
+        overloadingMain.add();
+        overloadingMain.add(2);
+        overloadingMain.add(2,3);
+        overloadingMain.add(2,3,5);
+    }
+}
